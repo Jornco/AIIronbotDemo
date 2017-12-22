2cedb3aafe0f43cacd7da7e8b7426d99465f3f25
action-hong <kkopitehong@gmail.com>
2017-12-22 10:58:49 +0800

    添加主界面,添加蓝牙地址权限请求

--- app/src/main/java/com/jornco/aiironbotdemo/MainActivity.java
+++ app/src/main/java/com/jornco/aiironbotdemo/MainActivity.java
@@ -1,13 +1,61 @@
 package com.jornco.aiironbotdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int PERMISSION_REQUEST_COARSE_LOCATION = 0x111;
+
+    private final LinkedHashMap<String, Class<? extends Activity>> buttons = new LinkedHashMap<String, Class<? extends Activity>>() {
+        {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initPermission();
+    }
+
+    private void initPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if ((checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                    || (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                    ) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.ACCESS_FINE_LOCATION},
+                        PERMISSION_REQUEST_COARSE_LOCATION);
+            }
+        }
+    }
+
+    private void initView() {
+        ViewGroup container = (ViewGroup) findViewById(R.id.list);
+        for (final Map.Entry<String, Class<? extends Activity>> entry : buttons.entrySet()) {
+            Button button = new Button(this);
+            button.setText(entry.getKey());
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this, entry.getValue()));
+                }
+            });
+            container.addView(button);
+        }
     }
 }
